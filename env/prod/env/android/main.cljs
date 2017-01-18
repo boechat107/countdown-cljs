4aba22e8e3a5f0535639be491e3abbab63b111eb
Andre Ambrosio Boechat <boechat107@gmail.com>
2017-01-18 15:29:39 -0200

    First commit; project generated with re-natal

---
+++ env/prod/env/android/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.android.main
+  (:require [countdown.android.core :as core]))
+
+ (core/init)
+
+
---
+++ env/prod/env/ios/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.ios.main
+  (:require [countdown.ios.core :as core]))
+
+ (core/init)
+
+
