840d825b96cb081b691273977668651f7b00e605
Andre Ambrosio Boechat <boechat107@gmail.com>
2017-01-21 16:06:10 -0200

    Add reset button

--- src/countdown/android/core.cljs
+++ src/countdown/android/core.cljs
@@ -11,7 +11,7 @@
   (let [cnt (subscribe [:get-counter])
         running? (subscribe [:get-running-flag])]
     (fn []
-      [ui/view {:style {:flex-direction "column" :margin 40 :align-items "center"
+      [ui/view {:style {:flex-direction "column" :margin 40
                         :justify-content "center" :flex 1}}
        [ui/text {:style {:font-size 30 :font-weight "100" :margin-bottom 20
                          :text-align "center"}}
@@ -19,12 +19,14 @@
        [ui/text {:style {:font-size 30 :font-weight "100" :margin-bottom 20
                          :text-align "center"}}
         @cnt]
-       ;; Button.
-       ;; TODO - Reset button
-       [ui/btn {:title (if @running? "Stop" "Start")
-                :on-press #(if @running?
-                             (dispatch [:stop-countdown])
-                             (dispatch [:start-countdown]))}]])))
+       [ui/view {:style {:flex-direction "row" :justify-content "space-around"}}
+        ;; Start/Stop Button, different title and action accordingly to the app's state.
+        [ui/btn {:title (if @running? "Stop" "Start")
+                 :on-press #(if @running?
+                              (dispatch [:stop-countdown])
+                              (dispatch [:start-countdown]))}]
+        ;; Resets the app's state to its initial value.
+        [ui/btn {:title "Reset" :on-press #(dispatch [:initialize-db])}]]])))
 
 (defn init []
   ;; `dispatch-sync` is used only when the application is being started.
